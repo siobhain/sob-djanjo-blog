8df218b9f01b494ac4b462d735bd735b8e58d32b
siobhain <sobrien@live.ie>
2023-07-04 00:21:04 +0000

    fix typo in ALLOWED_HOSTS

--- theblogproj/settings.py
+++ theblogproj/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sob-blog.herokuapp.com ', '8000-siobhain-sobdjanjoblog-kjqp31z4e1m.ws-eu101.gitpod.io']
+ALLOWED_HOSTS = ['sob-blog.herokuapp.com', '8000-siobhain-sobdjanjoblog-kjqp31z4e1m.ws-eu101.gitpod.io']
 
 
 # Application definition
