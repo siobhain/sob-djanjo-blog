76b3bc877404bd7c40ba7d325db05c5dcefc631c
siobhain <sobrien@live.ie>
2023-07-20 19:18:25 +0000

    add PostList view to app urls & appurls to Project

--- theblogproj/urls.py
+++ theblogproj/urls.py
@@ -1,4 +1,4 @@
-"""theblogproj URL Configuration
+""" theblogproj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -19,4 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-]
+    path("", include("blogapp.urls"), name="blogapp-urls"),]
