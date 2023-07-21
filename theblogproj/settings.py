57b8c48bf6596ba07b637dbbd972ed57aeda9b09
siobhain <sobrien@live.ie>
2023-07-21 21:18:46 +0000

    Create CommentForm in forms using libs crispyV2 & crispy-bootstrap5

--- theblogproj/settings.py
+++ theblogproj/settings.py
@@ -51,8 +51,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'blogapp',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 SITE_ID = 1
 
